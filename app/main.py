a94764673ba0a41399bb4a57f7fdab971981199a
danchomas <danchomas@gmail.com>
2025-10-06 12:46:09 +0300

    base structure for project

---
+++ app/main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI(
+    title="Resonance Music App",
+    descriprion="Music App for new artists",
+)
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
