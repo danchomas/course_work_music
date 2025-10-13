9a15eb9f4a64b1d3878e21e693b85d9e1a510ae7
danchomas <danchomas@gmail.com>
2025-10-13 12:06:52 +0300

    profile managers

--- app/services/profile_services.py
+++ app/services/profile_services.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import Session
 from sqlalchemy.exc import IntegrityError
 from models.profile_model import Profile
 from sqlalchemy import select
-from schemas.profile_schemas import UserCreateSchema
+from schemas.profile_schemas import ProfileCreateSchema
 from uuid import UUID
 from fastapi import HTTPException, status
 
@@ -12,19 +12,14 @@ class ProfileCreateManager:
     
     def create_profile(self, profile: ProfileCreateSchema) -> Profile:
         existing_profile = self.db.query(Profile).filter(
-            (Profile.nickname == profile.nikcname) | (Profile.user_id == profile.user_id)
+            (Profile.nickname == profile.nickname)
         ).first()
 
         if existing_profile:
-            if Profile.nickname == profile.nikcname:
+            if Profile.nickname == profile.nickname:
                 raise HTTPException(
                     status_code=401,
                     detail="К сожалению исполнитель с таким никнеймом уже создан!"
-                )
-            if Profile.user_id == profile.user_id:
-                raise HTTPException(
-                    status_code=401,
-                    detail="Каждый пользователь может быть одновременно только одним исполнителем"
                 )
         db_profile = Profile(
             nickname=profile.nickname,
@@ -33,3 +28,11 @@ class ProfileCreateManager:
         self.db.add(db_profile)
         self.db.commit()
         self.db.refresh(db_profile)
+        return db_profile
+
+class ProfileGetManager():
+    def __init(self, db: Session):
+        self.db = db
+    
+    def get_all_profiles(self) -> list[Profile]:
+        return self.db.query(Profile).all()
