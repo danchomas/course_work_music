c50712ebf42eed59fbe505eb15551f62bd9d6e6c
danchomas <danchomas@gmail.com>
2025-10-18 16:08:27 +0300

    new optionsl

--- app/schemas/profile_schemas.py
+++ app/schemas/profile_schemas.py
@@ -1,31 +1,32 @@
-from pydantic import BaseModel, EmailStr, Field, field_validator, ConfigDict
-from typing import Optional
+from pydantic import BaseModel, Field, field_validator, ConfigDict
 import re
 
-class ProfileSchema(BaseModel):
-    nickname: str = Field(
-        min_length = 3,
-        max_length = 20
-    )
 
-    @field_validator('nickname')
+class ProfileSchema(BaseModel):
+    nickname: str = Field(min_length=3, max_length=20)
+
+    @field_validator("nickname")
     @classmethod
     def validate_username(cls, value: str) -> str:
         if not re.match(r"^[a-zA-Zа-яА-Я0-9_ -]+$", value):
-            raise ValueError('Никнейм должен содержать только русские/английские символы, а так же цифры, пробелы и подчеркивания')
-        if value.startswith(' ') or value.endswith(' '):
-            raise ValueError('Нельзя начинать или заканчивать ник проблелом')
+            raise ValueError(
+                "Никнейм должен содержать только русские/английские символы, а так же цифры, пробелы и подчеркивания"
+            )
+        if value.startswith(" ") or value.endswith(" "):
+            raise ValueError("Нельзя начинать или заканчивать ник проблелом")
         return value
 
-class ProfileCreateSchema(ProfileSchema):
-    bio: Optional[str] = None
 
-    @field_validator('bio')
+class ProfileCreateSchema(ProfileSchema):
+    bio: str | None
+
+    @field_validator("bio")
     @classmethod
     def validate_bio(cls, value: str) -> str:
         if len(value) > 200:
-            raise ValueError('Описание профиля должно содержать не больше 200 символов')
+            raise ValueError("Описание профиля должно содержать не больше 200 символов")
         return value
+
 
 class ProfileUserSchema(ProfileSchema):
     user_id: int
