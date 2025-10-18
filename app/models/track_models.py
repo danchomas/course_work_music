300fbd7b9aece3a7b2a6064ebbe8f8c473e48e8f
danchomas <danchomas@gmail.com>
2025-10-18 15:53:56 +0300

    update title

--- app/models/track_models.py
+++ app/models/track_models.py
@@ -7,7 +7,7 @@ class Track(Base):
     __tablename__ = "tracks"
 
     id = Column(UUID(as_uuid=True), primary_key=True, nullable=False)
-    name = Column(String)
+    title = Column(String)
     cover_url = Column(String, unique=True)
     music_file_url = Column(String, unique=True)
     owner = Column(Integer, ForeignKey="profiles.id")
