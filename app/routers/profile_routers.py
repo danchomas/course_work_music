e119e90a61b85460f7ee29656f0dd0c9584308ca
danchomas <danchomas@gmail.com>
2025-10-18 15:22:26 +0300

    profiles fix

--- app/routers/profile_routers.py
+++ app/routers/profile_routers.py
@@ -6,35 +6,33 @@ from typing import List
 from schemas.profile_schemas import (
     ProfileCreateSchema,
     ProfileSchema,
-    ProfileUserSchema
+    ProfileUserSchema,
 )
-from services.profile_services import (
-    ProfileCreateManager,
-    ProfileGetManager
-)
+from services.profile_services import ProfileCreateManager, ProfileGetManager
 from core.database import get_db
 from core.security import auth
 
 router = APIRouter()
 
+
 @router.post("/create_profile", dependencies=[Security(auth.verify_token)])
 def create_profile(
     new_profile: ProfileCreateSchema = Body(...),
-    db: Session = Depends(get_db)
+    db: Session = Depends(get_db),
+    payload: dict = Depends(auth.verify_token),
 ) -> ProfileSchema:
-    profile = ProfileCreateManager(db).create_profile(new_profile)
+    profile = ProfileCreateManager(db).create_profile(new_profile, payload["id"])
     return profile
 
+
 @router.get("/get_all_profiles")
-def get_all_profiles(
-    db: Session = Depends(get_db)
-) -> List[ProfileUserSchema]:
+def get_all_profiles(db: Session = Depends(get_db)) -> List[ProfileUserSchema]:
     return ProfileGetManager(db).get_all_profiles()
+
 
 @router.post("/get_my_profile")
 def get_my_profile(
-    db: Session = Depends(get_db),
-    payload: dict = Depends(auth.verify_token)
+    db: Session = Depends(get_db), payload: dict = Depends(auth.verify_token)
 ) -> ProfileUserSchema:
     user_id = payload.get("id")
     return ProfileGetManager(db).get_user_profile(user_id)
--- app/services/profile_services.py
+++ app/services/profile_services.py
@@ -6,42 +6,44 @@ from schemas.profile_schemas import ProfileCreateSchema
 from uuid import UUID
 from fastapi import HTTPException, status
 
+
 class ProfileCreateManager:
     def __init__(self, db: Session):
         self.db = db
-    
-    def create_profile(self, profile: ProfileCreateSchema) -> Profile:
-        existing_profile = self.db.query(Profile).filter(
-            (Profile.nickname == profile.nickname)
-        ).first()
+
+    def create_profile(self, profile: ProfileCreateSchema, user_id: int) -> Profile:
+        existing_profile = (
+            self.db.query(Profile)
+            .filter((Profile.nickname == profile.nickname))
+            .first()
+        )
 
         if existing_profile:
             if Profile.nickname == profile.nickname:
                 raise HTTPException(
                     status_code=401,
-                    detail="К сожалению исполнитель с таким никнеймом уже создан!"
+                    detail="К сожалению исполнитель с таким никнеймом уже создан!",
                 )
         db_profile = Profile(
-            nickname=profile.nickname,
-            bio=profile.bio
+            nickname=profile.nickname, bio=profile.bio, user_id=user_id
         )
         self.db.add(db_profile)
         self.db.commit()
         self.db.refresh(db_profile)
         return db_profile
 
-class ProfileGetManager():
+
+class ProfileGetManager:
     def __init__(self, db: Session):
         self.db = db
-    
+
     def get_all_profiles(self) -> list[Profile]:
         return self.db.query(Profile).all()
-    
+
     def get_user_profile(self, user_id: int) -> Profile:
         db_profile = self.db.query(Profile).filter(Profile.user_id == user_id).first()
         if not db_profile:
             raise HTTPException(
-                status_code=401,
-                detail="Вы пока не зарегистрировали себя как артист"
+                status_code=401, detail="Вы пока не зарегистрировали себя как артист"
             )
         return db_profile
