27163d1ee40756a5aaa125fa696d7ddaa634c4a5
danchomas <danchomas@gmail.com>
2025-10-19 18:36:39 +0300

    track models update

--- app/models/track_models.py
+++ app/models/track_models.py
@@ -1,13 +1,13 @@
 from sqlalchemy import Column, String, Integer, Enum, ForeignKey
 from core.database import Base
 from sqlalchemy.dialects.postgresql import UUID
+import uuid
 
 
 class Track(Base):
     __tablename__ = "tracks"
 
-    id = Column(UUID(as_uuid=True), primary_key=True, nullable=False)
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     title = Column(String, nullable=False)
-    cover_url = Column(String, unique=True, nullable=False)
     music_file_url = Column(String, unique=True, nullable=False)
-    owner = Column(Integer, ForeignKey="profiles.id", nullable=False)
+    owner = Column(Integer, ForeignKey("profiles.id"), nullable=False)
