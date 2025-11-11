02bf1bba2016fbd9ac933a470e1400ffa1a0b69b
danchomas <danchomas@gmail.com>
2025-11-11 18:39:49 +0300

    get playlists

--- app/routers/playlist_routers.py
+++ app/routers/playlist_routers.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends, Body, Form
-from services.playlist_services import PlaylistCreateManager
+from services.playlist_services import PlaylistCreateManager, PlaylistGetManager
 from sqlalchemy.orm import Session
 from core.database import get_db
 from core.security import auth
@@ -17,3 +17,18 @@ def create_playlist(
     user_id = payload.get("id")
     playlist = PlaylistCreateManager(db).create_playlis(title, description, user_id)
     return playlist
+
+
+@router.get("/playlists/get_all")
+def get_all_playlists(
+    db: Session = Depends(get_db),
+):
+    return PlaylistGetManager(db).get_all_playlists()
+
+
+@router.get("/playlists/get_my_playlists")
+def get_my_playlists(
+    db: Session = Depends(get_db),
+    payload: dict = Depends(auth.verify_token),
+):
+    return PlaylistGetManager(db).get_my_playlists(payload.get("id"))
--- app/services/playlist_services.py
+++ app/services/playlist_services.py
@@ -7,9 +7,23 @@ class PlaylistCreateManager:
     def __init__(self, db: Session):
         self.db = db
 
-    def create_playlis(self, title: str, description: str, user_id: str):
+    def create_playlis(self, title: str, description: str, user_id: int):
         db_playlist = Playlist(title=title, description=description, owner_id=user_id)
         self.db.add(db_playlist)
         self.db.commit()
         self.db.refresh(db_playlist)
         return db_playlist
+
+
+class PlaylistGetManager:
+    def __init__(self, db: Session):
+        self.db = db
+
+    def get_all_playlists(self):
+        playlists = self.db.query(Playlist).all()
+        return playlists
+
+    def get_my_playlists(self, user_id: int):
+        if user_id:
+            return self.db.query(Playlist).filter(Playlist.owner_id == user_id).all()
+        raise HTTPException(status_code=404, detail="у вас еще не нашлось плейлистов")
