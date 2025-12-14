d8b17e37b0d933ad4e168f8b713c85841fc4abc0
danchomas <danchomas@gmail.com>
2025-12-14 14:28:17 +0300

    title managers add for cover xd xd

---
+++ app/services/title_services.py
@@ -0,0 +1,28 @@
+import uuid
+import os
+from sqlalchemy.orm import Session
+from models.title_models import Title
+from models.profile_model import Profile
+from fastapi import HTTPException
+
+BASE_DIR = "files"
+
+class CoverCreateService:
+    def __init__(self, db: Session):
+        self.db = db
+
+    def create_cover_file(self, artist_nickname, file, track, album_id) -> uuid:
+        cover_file_path = os.path.join(
+            BASE_DIR, artist_nickname, track, file.filename
+        )
+        os.makedirs(os.path.dirname(cover_file_path), exist_ok=True)
+        with open(cover_file_path, "wb") as buffer:
+            buffer.write(file.file.read())
+        db_cover = Title(
+            track_id=album_id,
+            file_path=cover_file_path,
+        )
+        self.db.add(db_cover)
+        self.db.commit()
+        self.db.refresh(db_cover)
+        return db_cover.id
